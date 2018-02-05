06e7861325ee74b0400c24b8c1cb6ba82fc67ad8
Christopher Carter <christopher.carter.777@gmail.com>
2018-02-05 12:36:09 -0800

    initial commit

---
+++ CSCarterDotNet/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSCarterDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }  //Index
+
+        public ActionResult About()
+        {
+            return View("About");
+        }  //About
+
+        public ActionResult Contact()
+        {
+            return View("Contact");
+        }  //Contact
+
+        public ActionResult Portfolio()
+        {
+            return View("Portfolio");
+        }  //Portfolio
+
+        public ActionResult A5netcore()
+        {
+            return View("A5netcore");
+        }  //a5netcore
+    }  //class
+}  //namespace
